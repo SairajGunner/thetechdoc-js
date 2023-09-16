d86cf37bc113ebe8ac4de682891e663232616054
Sairaj Udayshankar <sairaj9396@gmail.com>
2023-09-16 16:35:31 -0400

    Updated files contains-includes

--- contains-includes/script.js
+++ contains-includes/script.js
@@ -1,25 +1,5 @@
-let inputText = undefined;
-let searchText = undefined;
-let result = "NO";
-
-document.onload = () => {
-  Array.prototype.includes = function (searchElement) {
+window.onload = function () {
+  String.prototype.includes = function (searchElement) {
     return this.indexOf(searchElement) > -1;
   };
-};
-
-changeInputText = () => {
-  inputText = document.getElementById("input-text").value;
-};
-
-search = () => {
-  searchText = document.getElementById("search-text").value;
-
-  try {
-    if (inputText.includes(searchText)) result = "YES!";
-    else result = "NO";
-    document.getElementById("result-heading").innerHTML = result;
-  } catch (e) {
-    document.getElementById("result-heading").innerHTML = "Error - " + e;
-  }
 };
